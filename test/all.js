31933ce13f3a3aa59a49d6c66a277a01359e78b0
Manfred Touron <m@42.am>
2015-03-13 19:42:24 +0100

    Added configuration test

--- test/all.js
+++ test/all.js
@@ -4,12 +4,28 @@ var _ = require('lodash'),
     chai = require('chai'),
     debug = require('debug')('tests'),
     Client = require('..'),
+    util = require('util'),
     should = chai.should();
 
 
 var inspect = function(name, obj) {
   debug(name, util.inspect(obj, {showHidden: false, depth: null}));
 };
+
+
+suite('#config', function() {
+  test('should have a minimal configuration', function() {
+    var client = new Client();
+    inspect(client.config);
+    client.config.should.have.property('api_endpoint');
+    client.config.should.have.property('access_key');
+    client.config.should.have.property('secret_token');
+  });
+  test('should have an url for api_endpoint', function() {
+    var client = new Client();
+    client.config.api_endpoint.should.contains('https://');
+  });
+});
 
 
 suite("[client]", function() {
